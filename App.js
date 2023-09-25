101bab4cda0b4bb2c2b0b8855bbe246971255194
VasylievYurii <uraplanetianin@gmail.com>
2023-09-25 20:24:18 +0300

    added fonts

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Keyboard } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { MainView } from "./App.styled";
 import RegistrationScreen from "./src/screens/RegistrationScreen/RegistrationScreen";
 import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
+import { Text } from "react-native";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import Entypo from "@expo/vector-icons/Entypo";
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        setAppIsReady(false);
+        await Font.loadAsync(Entypo.font);
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
 
   useEffect(() => {
     const keyboardDidShowListener = Keyboard.addListener(
@@ -26,8 +48,19 @@ export default function App() {
       keyboardDidHideListener.remove();
     };
   }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
-    <MainView>
+    <MainView onLayout={onLayoutRootView}>
       {/* <RegistrationScreen isKeyboardVisible={isKeyboardVisible} /> */}
       <LoginScreen isKeyboardVisible={isKeyboardVisible} />
       <StatusBar style="auto" />
